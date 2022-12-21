f13a0f4d1dec25a44d3b7762f366dc12727f8095
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-12-21 15:12:52 -0300

    ✨ feat: debugando a aplicação

--- chapter2/rentalx/src/server.ts
+++ chapter2/rentalx/src/server.ts
@@ -2,8 +2,16 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   return res.json({ message: 'Hello World!' });
+});
+
+app.post('/courses', (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  return res.json({ name });
 });
 
 app.listen(3333, () => {
