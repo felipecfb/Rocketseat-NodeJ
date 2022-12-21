39cb6cb6a046d4e1fbcf609acb9447fe7e1f7cb9
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-12-21 15:26:53 -0300

    ✨ feat: inserindo id com uuid

--- chapter2/rentalx/src/routes/categories.routes.ts
+++ chapter2/rentalx/src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Request, Response, Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (req: Request, res: Response) => {
+categoriesRoutes.post('/', (req: Request, res: Response) => {
   const { name, description } = req.body;
 
-  categories.push({
+  const category = {
+    id: uuidv4(),
     name,
     description,
-  });
+  };
+
+  categories.push(category);
 
   return res.status(201).send();
 });
