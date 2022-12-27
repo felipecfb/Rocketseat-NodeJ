a2cd914745d3cf3f6dde5b46e48a9bf4be0274e5
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-12-27 17:00:26 -0300

    ✨ feat: criando repositório de especificação

---
+++ chapter2/rentalx/src/routes/specifications.routes.ts
@@ -0,0 +1,22 @@
+import { Request, Response, Router } from 'express';
+
+import { SpecificationRepository } from '../modules/cars/repositories/SpecificationsRepository';
+import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationRepository();
+
+specificationsRoutes.post('/', (req: Request, res: Response) => {
+  const { name, description } = req.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository,
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return res.status(201).send();
+});
+
+export { specificationsRoutes };
