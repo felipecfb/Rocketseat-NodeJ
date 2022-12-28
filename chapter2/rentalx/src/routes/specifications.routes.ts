f541e7e8065bfcabbf2e6c1b8c92d5931f2e8c88
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-12-28 11:05:06 -0300

    ✨ feat: criando Use Case de especificação

--- chapter2/rentalx/src/routes/specifications.routes.ts
+++ chapter2/rentalx/src/routes/specifications.routes.ts
@@ -1,22 +1,11 @@
 import { Request, Response, Router } from 'express';
 
-import { SpecificationRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationRepository();
-
 specificationsRoutes.post('/', (req: Request, res: Response) => {
-  const { name, description } = req.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository,
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return res.status(201).send();
+  return createSpecificationController.handle(req, res);
 });
 
 export { specificationsRoutes };
