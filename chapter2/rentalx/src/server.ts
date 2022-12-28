0f29555bdd3f59ae2af885d9926a2b58bd557e44
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-12-28 10:39:26 -0300

    ✨ feat: criando Use Case de categoria

--- chapter2/rentalx/src/server.ts
+++ chapter2/rentalx/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
 import { categoriesRoutes } from './routes/categories.routes';
 import { specificationsRoutes } from './routes/specifications.routes';
