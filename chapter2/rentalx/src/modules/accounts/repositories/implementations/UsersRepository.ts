4c01b17e7db19b63524d113967af8291ab3aae59
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-01-10 14:41:37 -0300

    ✨ feat: criando repositório de usuário

---
+++ chapter2/rentalx/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,34 @@
+import { Repository } from 'typeorm';
+
+import { AppDataSource } from '../../../../database/data-source';
+import { User } from '../../../../database/entities/User';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(User);
+  }
+
+  async create({
+    name,
+    username,
+    email,
+    driver_license,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      email,
+      driver_license,
+      password,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
