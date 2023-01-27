e2bc9fe3d0ad39112f632d1483575614174729f9
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-01-27 11:16:12 -0300

    ✨ feat: Criando o use case de criação de carro usando TDD

--- chapter2/rentalx/src/modules/cars/repositories/ICarsRepository.ts
+++ chapter2/rentalx/src/modules/cars/repositories/ICarsRepository.ts
@@ -1,7 +1,10 @@
+import { Car } from '@database/infra/typeorm/entities/Car';
+
 import { ICreateCarDTO } from '../dtos/ICreateCarDTO';
 
 interface ICarsRepository {
-  create(data: ICreateCarDTO): Promise<void>;
+  create(data: ICreateCarDTO): Promise<Car>;
+  findByLicensePlate(license_plate: string): Promise<Car>;
 }
 
 export { ICarsRepository };
