10c6ef5f8865fc458a95980e57a58691d3628282
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-01-31 15:19:04 -0300

    ✨ feat: criando caso de uso do cadastro de imagens do carro

--- chapter2/rentalx/src/shared/infra/http/server.ts
+++ chapter2/rentalx/src/shared/infra/http/server.ts
@@ -27,6 +27,8 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     });
   }
 
+  console.log(err);
+
   return res.status(500).json({
     status: 'error',
     message: `Internal server error - ${err.message}`,
