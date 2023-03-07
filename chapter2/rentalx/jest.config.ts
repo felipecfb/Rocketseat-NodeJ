d51725a663732a649609163320c214e9527b0fc8
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-03-07 10:45:33 -0300

    ✨ feat: implementando o coverage report

--- chapter2/rentalx/jest.config.ts
+++ chapter2/rentalx/jest.config.ts
@@ -7,7 +7,15 @@ export default {
 
   clearMocks: true,
 
+  collectCoverage: true,
+
+  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
+
+  coverageDirectory: 'coverage',
+
   coverageProvider: 'v8',
+
+  coverageReporters: ['text-summary', 'lcov'],
 
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/src/',
